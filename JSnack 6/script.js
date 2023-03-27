592e8742f2d1dcd2c101b9e6ccb52307b7536a3b
GiacomoArena <giacomoarena28@gmail.com>
2023-03-27 16:11:45 +0200

    first commit

---
+++ JSnack 6/script.js
@@ -0,0 +1,34 @@
+/*JSnack 6
+ Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente. */
+
+
+
+for( let i =0; i < 3; i++){
+const numeroUtente = prompt("Inserisci un numero");
+cubo = Math.pow(numeroUtente, 3)
+console.log(` il cubo di ${numeroUtente} é ${cubo}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*while(numeroUtente){
+  console.log(` il cubo di ${numeroUtente} é ${cubo}`);
+}*/
